d4d3dab0b015fd25a97217fa57855f0cca6fc6cc
Fara <frabarisoa@gmail.com>
2022-05-06 15:52:36 +0200

    changes in pokemontrainer

--- src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonTrainerFactory.java
@@ -4,6 +4,7 @@ public class PokemonTrainerFactory implements IPokemonTrainerFactory{
 
 	@Override
 	public PokemonTrainer createTrainer(String name, Team team, IPokedexFactory pokedexFactory) {
+		return null;
 		
 	}
 
